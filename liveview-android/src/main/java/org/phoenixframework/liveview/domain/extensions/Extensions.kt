98dc15890291302f9939155d9d7a27cfc25b37b1
Nelson Glauber <nelson.glauber@dockyard.com>
2023-12-22 10:08:12 -0300

    Adding support for system colors

--- liveview-android/src/main/java/org/phoenixframework/liveview/domain/extensions/Extensions.kt
+++ liveview-android/src/main/java/org/phoenixframework/liveview/domain/extensions/Extensions.kt
@@ -6,11 +6,34 @@ import androidx.core.text.isDigitsOnly
 fun String.isNotEmptyAndIsDigitsOnly(): Boolean = this.isNotEmpty() && this.isDigitsOnly()
 
 internal fun String.toColor(): Color {
-    val hexColorString = this
-        .removePrefix("0x")
-        .removePrefix("#")
-    val colorLong = hexColorString.toLong(16)
-    return Color(colorLong)
+    return when (this) {
+        "system-red" -> Color.Red
+        "system-green" -> Color.Green
+        "system-blue" -> Color.Blue
+        "system-black" -> Color.Black
+        "system-white" -> Color.White
+        "system-yellow" -> Color.Yellow
+        "system-magenta" -> Color.Magenta
+        "system-gray" -> Color.Gray
+        "system-light-gray" -> Color.LightGray
+        "system-dark-gray" -> Color.DarkGray
+        "system-transparent" -> Color.Transparent
+        "system-cyan" -> Color.Cyan
+        else -> {
+            var hexColorString = this
+                .removePrefix("0x")
+                .removePrefix("#")
+            // We're going to allow passing in RRGGBB or AARRGGBB
+            if (hexColorString.length == 6)
+                hexColorString = "FF$hexColorString"
+            try {
+                Color(hexColorString.toLong(16))
+            } catch (e: Exception) {
+                Color.Unspecified
+            }
+        }
+    }
+
 }
 
 // TODO This extension is using reflexion to get default values for different component's properties
