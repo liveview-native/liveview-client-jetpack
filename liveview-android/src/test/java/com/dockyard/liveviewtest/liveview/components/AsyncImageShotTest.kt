f3ba9e82757949d2680d21b89e3344a2f5c09a3c
Nelson Glauber <nelson.glauber@dockyard.com>
2024-01-09 15:34:31 -0300

    Fixed AsyncImage test

--- liveview-android/src/test/java/com/dockyard/liveviewtest/liveview/components/AsyncImageShotTest.kt
+++ liveview-android/src/test/java/com/dockyard/liveviewtest/liveview/components/AsyncImageShotTest.kt
@@ -1,17 +1,39 @@
 package com.dockyard.liveviewtest.liveview.components
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import coil.Coil
+import coil.ImageLoader
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.AsyncImage
+import coil.test.FakeImageLoaderEngine
 import com.dockyard.liveviewtest.liveview.util.LiveViewComposableTest
+import org.junit.Before
 import org.junit.Test
 
 class AsyncImageShotTest : LiveViewComposableTest() {
+    private val url = "https://assets.dockyard.com/images/narwin-home-flare.jpg"
+
+    @OptIn(ExperimentalCoilApi::class)
+    @Before
+    fun before() {
+        // https://coil-kt.github.io/coil/testing/
+        val engine = FakeImageLoaderEngine.Builder()
+            .intercept(url, ColorDrawable(Color.RED))
+            .default(ColorDrawable(Color.BLUE))
+            .build()
+        val imageLoader = ImageLoader.Builder(composeRule.activity)
+            .components { add(engine) }
+            .build()
+        Coil.setImageLoader(imageLoader)
+    }
+
     @Test
     fun simpleAsyncImageLoadingTest() {
-        val url = "https://assets.dockyard.com/images/narwin-home-flare.jpg"
         compareNativeComposableWithTemplate(
             delayBeforeScreenshot = 2000,
             nativeComposable = {
