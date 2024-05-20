dff63536c6a01f924d3d56d905aff32dbd6bb5d4
Brian Cardarella <bcardarella@gmail.com>
2024-05-20 11:07:43 -0400

    Update Jetpack client for latest LVN

--- mix.exs
+++ mix.exs
@@ -31,10 +31,9 @@ defmodule LiveViewNative.Jetpack.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:floki, ">= 0.30.0", only: :test},
-      {:live_view_native, github: "liveview-native/live_view_native", tag: "v0.3.0-beta.1"},
-      {:live_view_native_stylesheet, github: "liveview-native/live_view_native_stylesheet", tag: "v0.3.0-beta.1"},
-      {:live_view_native_test, github: "liveview-native/live_view_native_test", tag: "v0.3.0", only: :test},
-      {:jason, "~> 1.2"},
+      {:live_view_native, github: "liveview-native/live_view_native", branch: "main", override: true},
+      {:live_view_native_stylesheet, github: "liveview-native/live_view_native_stylesheet", branch: "main", only: :test},
+      {:live_view_native_test, github: "liveview-native/live_view_native_test", branch: "main", only: :test},
       {:nimble_parsec, "~> 1.3"}
     ]
   end
--- test/live_view_native/jetpack/integrations/inline_test.exs
+++ test/live_view_native/jetpack/integrations/inline_test.exs
@@ -3,6 +3,7 @@ defmodule LiveViewNative.Jetpack.InlineRenderTest do
 
   import Phoenix.ConnTest
   import Phoenix.LiveViewTest
+  import LiveViewNativeTest
 
   @endpoint LiveViewNativeTest.Endpoint
 
@@ -11,13 +12,13 @@ defmodule LiveViewNative.Jetpack.InlineRenderTest do
   end
 
   test "can render the jetpack format", %{conn: conn} do
-    {:ok, lv, _body} = live(conn, "/inline?_format=jetpack")
+    {:ok, lv, _body} = native(conn, "/inline", :jetpack)
 
     assert lv |> element("text") |> render() =~ "Inline Jetpack Render 100"
   end
 
   test "can render the jetpack format with watch target", %{conn: conn} do
-    {:ok, lv, _body} = live(conn, "/inline?_format=jetpack&_interface[target]=watch")
+    {:ok, lv, _body} = native(conn, "/inline", :jetpack, %{"target" => "watch"})
 
     assert lv |> element("text") |> render() =~ "Watch Target Inline Jetpack Render 100"
   end
