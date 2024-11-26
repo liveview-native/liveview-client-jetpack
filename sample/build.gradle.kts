e213f9310ed4825b9a9c05d2e837b4fa7bab8fc2
Nelson Glauber <nelson.glauber@dockyard.com>
2024-11-26 15:28:16 -0300

    Fix duplicate dependency issue

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -18,7 +18,10 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -46,5 +49,6 @@ dependencies {
     implementation(project(Constants.moduleClientAddons))
     implementation(libs.live.form) {
         isChanging = true
+        exclude(group = Constants.publishGroupId, module = Constants.publishArtifactClientId)
     }
 }
